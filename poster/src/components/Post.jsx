2e5289649307e65b4ea9bfcd7f0bc788024ef7ce
Syartaa <syartapajaziti@gmail.com>
2023-12-01 20:06:38 -0800

    create componients post

---
+++ poster/src/components/Post.jsx
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/prop-types */
+// eslint-disable-next-line no-unused-vars
+
+import classes from './Post.module.css';
+
+ function Post(props) {
+
+  return (
+    <li className={classes.post}>
+      <p className={classes.author}>{props.author}</p>
+      <p className={classes.text}>{props.body}</p>
+    </li>
+  );
+}
+
+export default Post;
