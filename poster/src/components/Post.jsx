c211bc1a532919211a131d6f7376a0ccec9761e0
Syartaa <syartapajaziti@gmail.com>
2023-12-07 23:39:41 -0800

    post details

--- poster/src/components/Post.jsx
+++ poster/src/components/Post.jsx
@@ -2,14 +2,16 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line no-unused-vars
 
-import classes from './Post.module.css';
+import { Link } from "react-router-dom";
+import classes from "./Post.module.css";
 
- function Post(props) {
-
+function Post({id,author, body}) {
   return (
     <li className={classes.post}>
-      <p className={classes.author}>{props.author}</p>
-      <p className={classes.text}>{props.body}</p>
+      <Link to={id}>
+        <p className={classes.author}>{author}</p>
+        <p className={classes.text}>{body}</p>
+      </Link>
     </li>
   );
 }
--- poster/src/main.jsx
+++ poster/src/main.jsx
@@ -3,10 +3,12 @@ import ReactDOM from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
 import "./index.css";
-import NewPost, { action as newPostAction} from "./routes/NewPost";
+import NewPost, { action as newPostAction } from "./routes/NewPost";
 import RootLayout from "./routes/RootLayout";
-import Posts, { loader as postsLoader} from "./routes/Posts";
- 
+import Posts, { loader as postsLoader } from "./routes/Posts";
+import PostDetails, {
+  loader as postDetailsLoader,
+} from "./routes/PostDetails";
 
 const router = createBrowserRouter([
   {
@@ -16,8 +18,11 @@ const router = createBrowserRouter([
       {
         path: "/",
         element: <Posts />,
-        loader : postsLoader,
-        children: [{ path: "/create-post", element: <NewPost />, action: newPostAction }],
+        loader: postsLoader,
+        children: [
+          { path: "/create-post", element: <NewPost />, action: newPostAction },
+          { path: "/:id", element: <PostDetails />, loader: postDetailsLoader },
+        ],
       },
     ],
   },
